c22bcff1b0b26f3d91107d0fb7c1589d21aaad9b
vhludnev <slavikman@yahoo.com>
2022-01-03 10:42:20 +0200

    mongoose schemas created

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,41 @@
-const express = require('express')
-const products = require('./data/products')
+// const express = require('express')
+// const dotenv = require('dotenv')
+// const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
 app.get('/', (req, res) => res.send('API is running...'))
+
 app.get('/api/products', (req, res) => {
+  //const newProducts = products.map((prod) => {
+  //  const { _id, name, image } = prod
+  //  return { _id, name, image }
+  //})
+  //res.json(newProducts)
   res.json(products)
 })
+
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
+  const product = products.find((p) => p._id == req.params.id)
+  if (!product) res.status(404).send('Product Does Not Exist')
   res.json(product)
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+)
