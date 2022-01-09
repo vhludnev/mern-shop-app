02cf77b1e298844eb75e8af5f90339a8d848c5a9
vhludnev <slavikman@yahoo.com>
2022-01-09 18:03:49 +0200

    Implementing Redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,32 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { /* useState, */ useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 //import products from '../products'
-import axios from 'axios'
+//import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = (/* { match } */) => {
   const { id } = useParams()
   //const product = products.find((p) => p._id === id)
-  const [product, setProduct] = useState({})
+  //const [product, setProduct] = useState({})
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      //const { data } = await axios.get(`/api/products/${match.params.id}`)
-      const { data } = await axios.get(`/api/products/${id}`)
+  // useEffect(() => {
+  //   const fetchProduct = async () => {
+  //     //const { data } = await axios.get(`/api/products/${match.params.id}`)
+  //     const { data } = await axios.get(`/api/products/${id}`)
 
-      setProduct(data)
-    }
+  //     setProduct(data)
+  //   }
 
-    fetchProduct()
-  }, [id])
+  //   fetchProduct()
+  // }, [id])
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  useEffect(() => dispatch(listProductDetails(id)), [dispatch, id])
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row>
+      {/* <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -77,7 +86,67 @@ const ProductScreen = (/* { match } */) => {
             </ListGroup>
           </Card>
         </Col>
-      </Row>
+      </Row> */}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    className='btn-block'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   )
 }
