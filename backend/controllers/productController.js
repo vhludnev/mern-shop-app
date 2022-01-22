85fe7b0aad742e8fa8fc49cee017e989e0a6ddf7
vhludnev <slavikman@yahoo.com>
2022-01-22 21:14:12 +0200

    Deployment on Heroku

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public (Fetch all products)
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 4
+  const pageSize = 10
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword // t.i. whatever is after '?' (question mark)
